da314839647103527cb97312b666273a87029409
Kevin Hoffman <autodidaddict@users.noreply.github.com>
2023-12-19 16:33:00 -0500

    agent now reboots (halts) firecracker VM when it encounters a fatal error (#6)

--- nex-agent/nex-agent.go
+++ nex-agent/nex-agent.go
@@ -1,5 +1,17 @@
 package nexagent
 
+import (
+	"fmt"
+	"os"
+	"syscall"
+)
+
 const (
 	VERSION = "0.0.1"
 )
+
+func HaltVM(err error) {
+	// On the off chance the agent's log is captured from the vm
+	fmt.Fprintf(os.Stderr, "Terminating Firecracker VM due to fatal error: %s\n", err)
+	syscall.Reboot(syscall.LINUX_REBOOT_CMD_RESTART)
+}
