7174e6976bdb0673941e194c6b8d69ea0a097ea1
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-04-29 06:20:39 -0600

    `nex update` stage 1 (#200)
    * adds update command
    * updates binary in place

---
+++ nex/upgrade.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+func versionCheck() (string, error) {
+	if VERSION == "development" {
+		return "", nil
+	}
+
+	res, err := http.Get("https://api.github.com/repos/synadia-io/nex/releases/latest")
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	b, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	payload := make(map[string]interface{})
+	err = json.Unmarshal(b, &payload)
+	if err != nil {
+		return "", err
+	}
+
+	latestTag, ok := payload["tag_name"].(string)
+	if !ok {
+		return "", errors.New("error parsing tag_name")
+	}
+
+	if latestTag != VERSION {
+		fmt.Printf(`================================================================
+🎉 There is a newer version [v%s] of the NEX CLI available 🎉
+To update, run:
+     nex update
+================================================================
+
+`,
+			latestTag)
+	}
+
+	return latestTag, nil
+}
+
+func UpgradeNex(ctx context.Context, logger *slog.Logger, newVersion string) (string, error) {
+	nexPath, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+
+	f, err := os.Open(nexPath)
+	if err != nil {
+		return "", err
+	}
+
+	// copy binary backup
+	f_bak, err := os.Create(nexPath + ".bak")
+	if err != nil {
+		return "", err
+	}
+	defer os.Remove(nexPath + ".bak")
+	_, err = io.Copy(f_bak, f)
+	if err != nil {
+		return "", err
+	}
+	f_bak.Close()
+	f.Close()
+
+	restoreBackup := func() {
+		logger.Info("Restoring backup binary")
+		if err := os.Rename(nexPath+".bak", nexPath); err != nil {
+			logger.Error("Failed to restore backup binary", slog.Any("err", err))
+		}
+	}
+
+	_os := runtime.GOOS
+	arch := runtime.GOARCH
+
+	url := fmt.Sprintf("https://github.com/synadia-io/nex/releases/download/%s/nex_%s_%s_%s", newVersion, newVersion, _os, arch)
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to download nex: %s", resp.Status)
+	}
+
+	nexBinary, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	nex, err := os.Create(filepath.Join(filepath.Dir(nexPath), "nex"))
+	if err != nil {
+		restoreBackup()
+		return "", err
+	}
+
+	_, err = nex.Write(nexBinary)
+	if err != nil {
+		restoreBackup()
+		return "", err
+	}
+
+	h := sha256.New()
+	if _, err := io.Copy(h, nex); err != nil {
+		return "", err
+	}
+
+	shasum := hex.EncodeToString(h.Sum(nil))
+
+	logger.Debug("New binary downloaded", slog.String("sha256", shasum))
+	logger.Info("nex upgrade complete!", slog.String("new_version", newVersion))
+
+	return shasum, nil
+}
---
+++ nex/upgrade_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	shandler "github.com/jordan-rash/slog-handler"
+)
+
+func logger(t *testing.T) *slog.Logger {
+	t.Helper()
+	return slog.New(shandler.NewHandler())
+}
+
+func setEnvironment(t *testing.T) {
+	t.Helper()
+
+	tempPath := t.TempDir()
+	f, err := os.Create(filepath.Join(tempPath, "nex"))
+	if err != nil {
+		t.Fail()
+	}
+	f.Close()
+
+	err = os.Chmod(filepath.Join(tempPath, "nex"), 0775)
+	if err != nil {
+		t.Fail()
+	}
+
+	path := os.Getenv("PATH")
+	os.Setenv("PATH", tempPath+":"+path)
+}
+
+func TestUpdateNex(t *testing.T) {
+	setEnvironment(t)
+	log := logger(t)
+
+	testNexPath, _ := exec.LookPath("nex")
+	t.Log("nex path: " + testNexPath)
+	if !strings.HasPrefix(testNexPath, os.TempDir()) {
+		t.Log("bailing on update nex test so real env isnt affected")
+		t.SkipNow()
+	}
+
+	shasum, err := UpgradeNex(context.Background(), log, "0.2.1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(shasum)
+}
