c65ca994fb9dc161201077af6f3e0b564352e150
kt <kthomas@users.noreply.github.com>
2024-05-07 12:29:33 -0400

    Remove no sandbox check in windows process manager initializer

--- internal/node/processmanager/process_mgr_windows.go
+++ internal/node/processmanager/process_mgr_windows.go
@@ -4,7 +4,6 @@ package processmanager
 
 import (
 	"context"
-	"errors"
 	"log/slog"
 
 	"github.com/synadia-io/nex/internal/models"
@@ -18,11 +17,5 @@ func NewProcessManager(
 	telemetry *observability.Telemetry,
 	ctx context.Context,
 ) (ProcessManager, error) {
-	if config.NoSandbox {
-		log.Warn("⚠️  Sandboxing has been disabled! Workloads are spawned directly by agents")
-		log.Warn("⚠️  Do not run untrusted workloads in this mode!")
-		return NewSpawningProcessManager(log, config, telemetry, ctx)
-	}
-
-	return nil, errors.New("Process manager must be configured without sandboxing")
+	return NewSpawningProcessManager(log, config, telemetry, ctx)
 }
