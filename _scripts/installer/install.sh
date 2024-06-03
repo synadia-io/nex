363f54572e7ed5eaf90ce8ebccd420f2ed0d93c8
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-06-03 09:31:24 -0600

    adds options to install agent with cli
    Signed-off-by: Jordan Rash <15827604+jordan-rash@users.noreply.github.com>

--- _scripts/installer/install.sh
+++ _scripts/installer/install.sh
@@ -7,6 +7,15 @@ command -v jq >/dev/null 2>&1 || {
 	exit 1
 }
 
+with_agent=false
+while getopts ":with_agent:" opt; do
+	case ${opt} in
+	a)
+		with_agent=true
+		;;
+	esac
+done
+
 get_version() {
 	echo $(
 		command curl -L -s \
@@ -60,21 +69,31 @@ os=$(get_os)
 arch=$(get_arch)
 binary_version=$(get_version)
 file_name="nex_${binary_version}_${os}_${arch}"
+agent_file_name="nex-agent_${binary_version}_${os}_${arch}"
 asset_uri="https://github.com/synadia-io/nex/releases/download/${binary_version}/${file_name}"
+agent_asset_uri="https://github.com/synadia-io/nex/releases/download/${binary_version}/${agent_file_name}"
 
 downloadFolder="${TMPDIR:-/tmp}"
 mkdir -p ${downloadFolder}
 downloaded_file="${downloadFolder}/nex"
+downloaded_agent_file="${downloadFolder}/nex-agent"
 executable_folder="/usr/local/bin"
 
 echo "[1/3] Download ${asset_uri} to ${downloadFolder}"
 rm -f ${downloaded_file}
 curl --silent --fail --location --output "${downloaded_file}" "${asset_uri}"
+if [ "$with_agent" = true ]; then
+	curl --silent --fail --location --output "${downloaded_agent_file}" "${agent_asset_uri}"
+fi
 
 echo "[2/3] Install nex to ${executable_folder}"
 mv ${downloaded_file} ${executable_folder}
 exe=${executable_folder}/nex
 chmod +x ${exe}
+if [ "$with_agent" = true ]; then
+	mv ${downloaded_agent_file} ${executable_folder}
+	chmod +x ${executable_folder}/nex-agent
+fi
 
 echo "[3/3] Check environment variables"
 echo ""
