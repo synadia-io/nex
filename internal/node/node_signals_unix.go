047a13d5d3c6a82c0a77737ad3a712bc28a438da
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-06-14 11:59:46 -0600

    unlocks darwin
    Signed-off-by: Jordan Rash <15827604+jordan-rash@users.noreply.github.com>

---
+++ internal/node/node_signals_unix.go
@@ -0,0 +1,17 @@
+//go:build linux || darwin
+
+package nexnode
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func (n *Node) installSignalHandlers() {
+	n.log.Debug("installing signal handlers")
+	// both firecracker and the embedded NATS server(s) register signal handlers... wipe those so ours are the ones being used
+	signal.Reset(syscall.SIGINT, syscall.SIGTERM, syscall.SIGUSR1, syscall.SIGUSR2, syscall.SIGHUP)
+	n.sigs = make(chan os.Signal, 1)
+	signal.Notify(n.sigs, os.Interrupt, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+}
--- nex/node.go
+++ nex/node.go
@@ -1,5 +1,3 @@
-//go:build linux || windows
-
 package main
 
 import (
