26ad3a6603cf8e6865518a3be5f2b5a89f78bf66
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-06-27 12:48:43 -0600

    check path incrementally
    Signed-off-by: Jordan Rash <15827604+jordan-rash@users.noreply.github.com>

--- internal/node/preflight/validate.go
+++ internal/node/preflight/validate.go
@@ -12,20 +12,16 @@ import (
 
 type BinaryVerify struct {
 	BinName string
-	Error   PreflightError
 }
 
 type PreflightError error
 
 var (
-	ErrNexAgentNotFound            = errors.New("nex-agent binary not found")
-	ErrFirecrackerNotFound         = errors.New("firecracker binary not found")
-	ErrHostLocalPluginNotFound     = errors.New("host-local binary not found")
-	ErrPtpPluginNotFound           = errors.New("ptp binary not found")
-	ErrTcRedirectTapPluginNotFound = errors.New("tc-redirect-tap binary not found")
-	ErrRootFsNotFound              = errors.New("rootfs file not found")
-	ErrVmlinuxNotFound             = errors.New("vmlinux file not found")
-	ErrCNIConfigNotFound           = errors.New("cni config file not found")
+	ErrBinaryNotFound      = errors.New("nex-agent binary not found")
+	ErrBinaryNotExecutable = errors.New("nex-agent binary not executable")
+	ErrRootFsNotFound      = errors.New("rootfs file not found")
+	ErrVmlinuxNotFound     = errors.New("vmlinux file not found")
+	ErrCNIConfigNotFound   = errors.New("cni config file not found")
 
 	ErrNoSandboxRequired            = errors.New("no sandbox required for os")
 	ErrFailedToSatifyDependency     = errors.New("failed to satisfy dependency")
@@ -39,29 +35,77 @@ var (
 	ErrUserCanceledPreflight        = errors.New("user canceled preflight")
 
 	binVerify = []BinaryVerify{
-		{BinName: "firecracker", Error: ErrFirecrackerNotFound},
-		{BinName: "host-local", Error: ErrHostLocalPluginNotFound},
-		{BinName: "ptp", Error: ErrPtpPluginNotFound},
-		{BinName: "tc-redirect-tap", Error: ErrTcRedirectTapPluginNotFound},
+		{BinName: "firecracker"},
+		{BinName: "host-local"},
+		{BinName: "ptp"},
+		{BinName: "tc-redirect-tap"},
 	}
 )
 
 func Validate(config *models.NodeConfiguration, logger *slog.Logger) PreflightError {
 	var errs PreflightError
 	if config.NoSandbox {
-		nexAgentPath, err := exec.LookPath("nex-agent")
-		if err != nil {
-			errs = errors.Join(errs, ErrNexAgentNotFound)
+
+		if len(config.BinPath) > 0 {
+			for _, p := range config.BinPath {
+				e := filepath.Join(p, "nex-agent")
+				f, err := os.Stat(e)
+				if err != nil {
+					logger.Debug("nex-agent binary NOT found", slog.String("path", e))
+					errs = errors.Join(errs, ErrBinaryNotFound)
+				} else if f.Mode()&0100 == 0 {
+					logger.Debug("nex-agent binary NOT executable", slog.String("path", e))
+					errs = errors.Join(errs, ErrBinaryNotExecutable)
+				} else {
+					logger.Debug("nex-agent binary found", slog.String("path", e))
+				}
+			}
 		} else {
-			logger.Debug("nex-agent binary found", slog.String("path", nexAgentPath))
+			nexAgentPath, err := exec.LookPath("nex-agent")
+			if err != nil {
+				logger.Debug("nex-agent binary NOT found", slog.String("path", nexAgentPath))
+				errs = errors.Join(errs, ErrBinaryNotFound)
+			} else {
+				logger.Debug("nex-agent binary found", slog.String("path", nexAgentPath))
+			}
 		}
 		return errs
 	}
 
 	for _, bin := range binVerify {
+		if len(config.BinPath) > 0 && bin.BinName == "firecracker" {
+			for _, p := range config.BinPath {
+				e := filepath.Join(p, bin.BinName)
+				f, err := os.Stat(e)
+				if err != nil {
+					logger.Debug(bin.BinName+" binary NOT found", slog.String("path", e))
+					errs = errors.Join(errs, ErrBinaryNotFound)
+				} else if f.Mode()&0100 == 0 {
+					logger.Debug(bin.BinName+" binary NOT executable", slog.String("path", e))
+					errs = errors.Join(errs, ErrBinaryNotExecutable)
+				} else {
+					logger.Debug(bin.BinName+" binary found", slog.String("path", e))
+				}
+			}
+		} else if len(config.CNI.BinPath) > 0 && bin.BinName != "firecracker" {
+			for _, p := range config.CNI.BinPath {
+				e := filepath.Join(p, bin.BinName)
+				f, err := os.Stat(e)
+				if err != nil {
+					logger.Debug(bin.BinName+" binary NOT found", slog.String("path", e))
+					errs = errors.Join(errs, ErrBinaryNotFound)
+				} else if f.Mode()&0100 == 0 {
+					logger.Debug(bin.BinName+" binary NOT executable", slog.String("path", e))
+					errs = errors.Join(errs, ErrBinaryNotExecutable)
+				} else {
+					logger.Debug(bin.BinName+" binary found", slog.String("path", e))
+				}
+			}
+		}
 		binPath, err := exec.LookPath(bin.BinName)
 		if err != nil {
-			errs = errors.Join(errs, bin.Error)
+			logger.Debug(bin.BinName+" binary NOT found", slog.String("path", binPath))
+			errs = errors.Join(errs, ErrBinaryNotFound)
 		} else {
 			logger.Debug(bin.BinName+" binary found", slog.String("path", binPath))
 		}
