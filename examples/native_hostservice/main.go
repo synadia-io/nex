9ba697291ebc51373942d82c23cd456f9010a380
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-07-31 16:13:33 -0600

    cleanup
    Signed-off-by: Jordan Rash <15827604+jordan-rash@users.noreply.github.com>

--- examples/native_hostservice/main.go
+++ examples/native_hostservice/main.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/nats-io/nats.go"
 )
@@ -36,7 +34,6 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Failed to connect to NATS: %s\n", err)
 		return
 	}
-	setupSignalHandlers(nc)
 
 	_, err = nc.Subscribe("fundemo", func(msg *nats.Msg) {
 		fmt.Fprintf(os.Stdout, "Received message: %s\n", string(msg.Data))
@@ -47,25 +44,4 @@ func main() {
 	}
 
 	<-ctx.Done()
-}
-
-func setupSignalHandlers(nc *nats.Conn) {
-	go func() {
-		signal.Reset(syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
-
-		for {
-			switch s := <-c; {
-			case s == syscall.SIGTERM || s == os.Interrupt || s == syscall.SIGQUIT:
-				fmt.Fprintf(os.Stdout, "Caught signal [%s], requesting clean shutdown", s.String())
-				nc.Drain()
-				os.Exit(0)
-
-			default:
-				nc.Drain()
-				os.Exit(0)
-			}
-		}
-	}()
 }
