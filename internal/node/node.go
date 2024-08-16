884b1d7ef2abd08e533d5211d30a3f48f509ea80
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-08-16 07:29:56 -0600

    Revert "remove nosandbox default (#353)"
    This reverts commit e5e569cd9430bb38e92fb46caf2102fabf9a208b.

--- internal/node/node.go
+++ internal/node/node.go
@@ -500,9 +500,7 @@ func (n *Node) loadNodeConfig() error {
 		n.config.OtelMetricsPort = n.nodeOpts.OtelMetricsPort
 		n.config.OtelTraces = n.nodeOpts.OtelTraces
 		n.config.OtelTracesExporter = n.nodeOpts.OtelTracesExporter
-		if n.nodeOpts.NoSandbox != nil {
-			n.config.NoSandbox = *n.nodeOpts.NoSandbox
-		}
+		n.config.NoSandbox = n.nodeOpts.NoSandbox
 		for k, v := range n.nodeOpts.Tags {
 			n.config.Tags[k] = v
 		}
