ac7f4c1ac3ae1021afc459c1d50bb990b3c06517
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-09-12 15:15:17 -0600

    add simple test
    Signed-off-by: Jordan Rash <15827604+jordan-rash@users.noreply.github.com>

---
+++ node/node_test.go
@@ -0,0 +1,43 @@
+package node_test
+
+import (
+	"testing"
+
+	"github.com/nats-io/nats-server/v2/server"
+	"github.com/nats-io/nats.go"
+
+	"github.com/synadia-io/nex/node"
+)
+
+func startNatsServer(t testing.TB) *nats.Conn {
+	t.Helper()
+	opts := &server.Options{}
+	ns, err := server.NewServer(opts)
+	if err != nil {
+		t.Fatal("failed to start nats server", err)
+	}
+	defer ns.Shutdown()
+
+	go ns.Start()
+	nc, err := nats.Connect(ns.ClientURL())
+	if err != nil {
+		t.Fatal("failed to connect to nats server", err)
+	}
+
+	return nc
+}
+
+func TestDefaultConfigNodeValidation(t *testing.T) {
+	nc := startNatsServer(t)
+	defer nc.Close()
+
+	tDir := t.TempDir()
+
+	node, err := node.NewNexNode(nc, node.WithResourceDirectory(tDir))
+	if err != nil {
+		t.Fatal("failed to create new nex node", err)
+	}
+	if err := node.Validate(); err != nil {
+		t.Fatal("failed to validate nex node", err)
+	}
+}
