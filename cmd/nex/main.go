cc3a1b6d392d58b7b6bdfd3dd693a77cd815e4a7
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-09-13 15:30:44 -0400

    CLI Update | `node up` (#372)
    * initial stubbing of nex cli
    
    Signed-off-by: Jordan Rash <15827604+jordan-rash@users.noreply.github.com>

---
+++ cmd/nex/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/alecthomas/kong"
+)
+
+const (
+	Banner = `
+  	   ▐ ▄ ▄▄▄ . ▐▄• ▄ 
+	  •█▌▐█▀▄.▀· █▌█▌▪
+	  ▐█▐▐▌▐▀▀▪▄ ·██· 
+	  ██▐█▌▐█▄▄▌▪▐█·█▌
+	  ▀▀ █▪ ▀▀▀ •▀▀ ▀▀
+`
+)
+
+var (
+	VERSION   string = "0.0.0"
+	COMMIT    string = "development"
+	BUILDDATE string = time.Now().Format(time.RFC822)
+)
+
+func main() {
+	userConfigPath, err := os.UserConfigDir()
+	if err != nil {
+		userConfigPath = "."
+	}
+	userResourcePath := filepath.Join(userConfigPath, ".nex", "bin")
+
+	nex := new(NexCLI)
+	ctx := kong.Parse(nex,
+		kong.Name("nex"),
+		kong.Description("The NATS execution engine\n"+Banner),
+		kong.UsageOnError(),
+		kong.ConfigureHelp(kong.HelpOptions{Compact: true, NoExpandSubcommands: true, FlagsLast: true}),
+		kong.Configuration(kong.JSON),
+		kong.Vars{
+			"version":             fmt.Sprintf("%s [%s] | Built: %s", VERSION, COMMIT, BUILDDATE),
+			"versionOnly":         VERSION,
+			"defaultResourcePath": userResourcePath,
+		},
+	)
+
+	ctx.BindTo(context.Background(), (*context.Context)(nil))
+	ctx.BindTo(nex.Globals, (*Globals)(nil))
+
+	err = ctx.Run()
+	ctx.FatalIfErrorf(err)
+}
