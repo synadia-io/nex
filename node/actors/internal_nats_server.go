034c08ab2894933aa420faf8fdac7af12b3a7253
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-09-17 10:13:39 -0400

    initial scaffolding for ergo supervision tree (#377)
    * Ergo Supervision tree
    
    Signed-off-by: Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
    Co-authored-by: Kevin Hoffman <autodidaddict@users.noreply.github.com>

---
+++ node/actors/internal_nats_server.go
@@ -0,0 +1,102 @@
+package actors
+
+import (
+	"ergo.services/ergo/act"
+	"ergo.services/ergo/gen"
+	"github.com/nats-io/nkeys"
+	"github.com/synadia-io/nex/models"
+)
+
+func createInternalNatsServer() gen.ProcessBehavior {
+	return &internalNatsServer{}
+}
+
+type internalNatsServer struct {
+	act.Actor
+	tokens        map[gen.Atom]gen.Ref
+	haveConsumers bool
+
+	creds       []agentCredential
+	nodeOptions models.NodeOptions
+}
+
+type agentCredential struct {
+	workloadType string
+	nkey         nkeys.KeyPair
+}
+
+func (ns *internalNatsServer) Init(args ...any) error {
+	ns.tokens = make(map[gen.Atom]gen.Ref)
+
+	ns.Log().Info("Internal NATS server started")
+
+	ns.nodeOptions = args[0].(models.NodeOptions)
+
+	creds, err := ns.buildAgentCredentials()
+	if err != nil {
+		return err
+	}
+	ns.creds = creds
+	err = ns.startNatsServer(creds)
+	if err != nil {
+		return err
+	}
+	ns.Send(ns.PID(), "post_init")
+
+	return nil
+}
+
+func (ns *internalNatsServer) HandleMessage(from gen.PID, message any) error {
+	eventStart := gen.MessageEventStart{Name: InternalNatsServerReadyName}
+	eventStop := gen.MessageEventStop{Name: InternalNatsServerReadyName}
+
+	switch message {
+	case "post_init":
+		evOptions := gen.EventOptions{
+			// NOTE: notify true allows us to deterministically wait until we have
+			// a consumer before we publish an event. No more sleep-and-hope pattern.
+			Notify: true,
+		}
+		token, err := ns.RegisterEvent(InternalNatsServerReadyName, evOptions)
+		if err != nil {
+			return err
+		}
+		ns.tokens[InternalNatsServerReadyName] = token
+		ns.Log().Info("registered publishable event %s, waiting for consumers...", InternalNatsServerReadyName)
+	case eventStart:
+		ns.Log().Info("publisher got first consumer for %s. start producing events...", InternalNatsServerReadyName)
+		ns.haveConsumers = true
+		err := ns.SendEvent(InternalNatsServerReadyName, ns.tokens[InternalNatsServerReadyName], InternalNatsServerReadyEvent{AgentCredentials: ns.creds})
+		if err != nil {
+			return err
+		}
+	case eventStop: // handle gen.MessageEventStop message
+		ns.Log().Info("no consumers for %s", InternalNatsServerReadyName)
+		ns.haveConsumers = false
+	}
+	return nil
+}
+
+// HandleInspect invoked on the request made with gen.Process.Inspect(...)
+func (ns *internalNatsServer) HandleInspect(from gen.PID, item ...string) map[string]string {
+	ns.Log().Info("internal nats server got inspect request from %s", from)
+	return nil
+}
+
+func (ns *internalNatsServer) buildAgentCredentials() ([]agentCredential, error) {
+	creds := make([]agentCredential, len(ns.nodeOptions.WorkloadOptions))
+	for i, w := range ns.nodeOptions.WorkloadOptions {
+		kp, _ := nkeys.CreateUser()
+		creds[i] = agentCredential{
+			workloadType: w.Name,
+			nkey:         kp,
+		}
+	}
+	return creds, nil
+}
+
+func (ns *internalNatsServer) startNatsServer(creds []agentCredential) error {
+	ns.Log().Debug("Starting internal NATS server")
+	// TODO!
+	return nil
+}
--- node/node_test.go
+++ node/node_test.go
@@ -1,12 +1,14 @@
 package node_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/nats-io/nats-server/v2/server"
 	"github.com/nats-io/nats.go"
 
+	"github.com/synadia-io/nex/models"
 	"github.com/synadia-io/nex/node"
 )
 
@@ -38,11 +40,36 @@ func TestDefaultConfigNodeValidation(t *testing.T) {
 
 	tDir := t.TempDir()
 
-	node, err := node.NewNexNode(nc, node.WithResourceDirectory(tDir), node.WithWorkloadTypes([]node.WorkloadOptions{{Name: "test", AgentUri: "https://derp.com", Argv: []string{}, Env: map[string]string{}}}))
+	node, err := node.NewNexNode(nc, models.WithResourceDirectory(tDir),
+		models.WithWorkloadTypes([]models.WorkloadOptions{{Name: "test", AgentUri: "https://derp.com", Argv: []string{}, Env: map[string]string{}}}))
 	if err != nil {
 		t.Fatal("failed to create new nex node", err)
 	}
 	if err := node.Validate(); err != nil {
 		t.Fatal("failed to validate nex node", err)
+	}
+}
+
+func TestNilOptions(t *testing.T) {
+	nc := startNatsServer(t)
+	defer nc.Close()
+
+	_, err := node.NewNexNode(nc, nil)
+	if err.Error() != `node required at least 1 workload type be configured in order to start
+resource directory does not exist` {
+		fmt.Printf("'%s'", err.Error())
+		t.Fatal("failed to return expected errors")
+	}
+}
+
+func TestNoOptions(t *testing.T) {
+	nc := startNatsServer(t)
+	defer nc.Close()
+
+	_, err := node.NewNexNode(nc)
+	if err.Error() != `node required at least 1 workload type be configured in order to start
+resource directory does not exist` {
+		fmt.Printf("'%s'", err.Error())
+		t.Fatal("failed to return expected errors")
 	}
 }
