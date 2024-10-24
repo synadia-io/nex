1aab2ef92734da7baf62b3e9260609165d96aa1f
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-10-24 06:44:01 -0600

    fix test broken during pair
    Signed-off-by: Jordan Rash <15827604+jordan-rash@users.noreply.github.com>

--- node/node_test.go
+++ node/node_test.go
@@ -47,7 +47,7 @@ func TestDefaultConfigNodeValidation(t *testing.T) {
 
 	tDir := t.TempDir()
 
-	node, err := node.NewNexNode(kp, nc, models.WithResourceDirectory(tDir),
+	node, err := node.NewNexNode(kp, nc, models.WithResourceDirectory(tDir), models.WithDisableDirectStart(true),
 		models.WithExternalAgents([]models.AgentOptions{{Name: "test", Uri: "https://derp.com", Configuration: json.RawMessage{}}}))
 	if err != nil {
 		t.Fatal("failed to create new nex node", err)
@@ -66,7 +66,7 @@ func TestNilOptions(t *testing.T) {
 		t.Fatal("failed to create keypair")
 	}
 
-	_, err = node.NewNexNode(kp, nc, nil)
+	_, err = node.NewNexNode(kp, nc, nil, models.WithDisableDirectStart(true))
 	if err.Error() != `node required at least 1 workload type be configured in order to start
 resource directory does not exist` {
 		fmt.Printf("'%s'", err.Error())
@@ -83,7 +83,7 @@ func TestNoOptions(t *testing.T) {
 		t.Fatal("failed to create keypair")
 	}
 
-	_, err = node.NewNexNode(kp, nc)
+	_, err = node.NewNexNode(kp, nc, models.WithDisableDirectStart(true))
 	if err.Error() != `node required at least 1 workload type be configured in order to start
 resource directory does not exist` {
 		fmt.Printf("'%s'", err.Error())
