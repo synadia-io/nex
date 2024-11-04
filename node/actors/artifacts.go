271c00a281b02bfb9f579cd0ef9cc5e67f23a371
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-11-04 15:23:35 -0700

    oci artifact support
    Signed-off-by: Jordan Rash <15827604+jordan-rash@users.noreply.github.com>

--- node/actors/artifacts.go
+++ node/actors/artifacts.go
@@ -1,13 +1,23 @@
 package actors
 
 import (
+	"context"
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"net/url"
 	"os"
 	"strings"
+
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
+	"oras.land/oras-go/v2"
+	"oras.land/oras-go/v2/content/memory"
+	"oras.land/oras-go/v2/registry/remote"
+	"oras.land/oras-go/v2/registry/remote/auth"
+	"oras.land/oras-go/v2/registry/remote/retry"
 )
 
 const (
@@ -59,11 +69,13 @@ func getArtifact(name string, uri string) (*ArtifactReference, error) {
 	case SchemeFile:
 		return cacheFile(name, location)
 	case SchemeNATS:
+		return nil, nil
 		//return cacheObjectStoreArtifact(name, extractTag(location), location)
 	case SchemeOCI:
-		//return cacheOciArtifact(name, extractTag(location), location)
+		return cacheOciArtifact(name, location)
+	default:
+		return nil, errors.New("unsupported artifact scheme")
 	}
-	return nil, nil
 }
 
 func cacheFile(name string, location *url.URL) (*ArtifactReference, error) {
@@ -82,34 +94,44 @@ func cacheFile(name string, location *url.URL) (*ArtifactReference, error) {
 	if err != nil {
 		return nil, err
 	}
+	defer fOrig.Close()
 
 	fCache, err := os.CreateTemp(os.TempDir(), "workload-*")
 	if err != nil {
 		return nil, err
 	}
+	defer fCache.Close()
 
-	_, err = io.Copy(fCache, fOrig)
+	if _, err = io.Copy(fCache, fOrig); err != nil {
+		return nil, err
+	}
+
+	fCacheInfo, err := fCache.Stat()
 	if err != nil {
 		return nil, err
 	}
 
-	fOrig.Close()
+	if _, err := fCache.Seek(0, io.SeekStart); err != nil {
+		return nil, err
+	}
 
-	cacheBytes, err := io.ReadAll(fCache)
+	hasher := sha256.New()
+	if _, err := io.Copy(hasher, fCache); err != nil {
+		return nil, err
+	}
+
+	err = os.Chmod(fCache.Name(), 0755)
 	if err != nil {
 		return nil, err
 	}
-	hash := sha256.New()
-	hash.Write(cacheBytes)
-	fCache.Close()
 
 	return &ArtifactReference{
 		Name:             name,
 		Tag:              tag,
 		OriginalLocation: location,
 		LocalCachePath:   fCache.Name(),
-		Digest:           hex.EncodeToString(hash.Sum(nil)),
-		Size:             len(cacheBytes),
+		Digest:           hex.EncodeToString(hasher.Sum(nil)),
+		Size:             int(fCacheInfo.Size()),
 	}, nil
 }
 
@@ -118,8 +140,95 @@ func cacheObjectStoreArtifact(name string, tag string, location *url.URL) (*Arti
 	return nil, nil
 }
 
-func cacheOciArtifact(name string, tag string, location *url.URL) (*ArtifactReference, error) {
-	// TODO: implement
-	// NOTE: for now let's assume that if the target registry requires auth, it will be supplied in the location.User field
-	return nil, nil
+func cacheOciArtifact(name string, location *url.URL) (*ArtifactReference, error) {
+	// TODO/NOTE: for now let's assume that if the target registry requires auth, it will be supplied in the location.User field
+
+	artifact, tag := parsePathTag(location)
+	repo, err := remote.NewRepository(fmt.Sprintf("%s%s", location.Host, artifact))
+	if err != nil {
+		return nil, err
+	}
+	repo.PlainHTTP = true
+	repo.Client = &auth.Client{
+		Client: retry.DefaultClient,
+		Cache:  auth.NewCache(),
+	}
+
+	store := memory.New()
+	descriptor, err := oras.Copy(context.TODO(), repo, tag, store, "", oras.DefaultCopyOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	if found, err := store.Exists(context.TODO(), descriptor); err != nil || !found {
+		return nil, errors.New("artifact not found")
+	}
+
+	rc, err := store.Fetch(context.TODO(), descriptor)
+	if err != nil {
+		return nil, err
+	}
+	defer rc.Close()
+
+	fetchData, err := io.ReadAll(rc)
+	if err != nil {
+		return nil, err
+	}
+
+	var manifest v1.Manifest
+	err = json.Unmarshal(fetchData, &manifest)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, layer := range manifest.Layers {
+		if layer.MediaType != "application/nex.artifact.binary" {
+			continue
+		}
+
+		l, err := repo.Fetch(context.TODO(), layer)
+		if err != nil {
+			return nil, err
+		}
+		defer l.Close()
+
+		fCache, err := os.CreateTemp(os.TempDir(), "workload-*")
+		if err != nil {
+			return nil, err
+		}
+
+		if _, err = io.Copy(fCache, l); err != nil {
+			return nil, err
+		}
+		defer fCache.Close()
+
+		fCacheInfo, err := fCache.Stat()
+		if err != nil {
+			return nil, err
+		}
+
+		if _, err := fCache.Seek(0, io.SeekStart); err != nil {
+			return nil, err
+		}
+
+		hasher := sha256.New()
+		if _, err := io.Copy(hasher, fCache); err != nil {
+			return nil, err
+		}
+
+		err = os.Chmod(fCache.Name(), 0755)
+		if err != nil {
+			return nil, err
+		}
+
+		return &ArtifactReference{
+			Name:             name,
+			Tag:              tag,
+			OriginalLocation: location,
+			LocalCachePath:   fCache.Name(),
+			Digest:           hex.EncodeToString(hasher.Sum(nil)),
+			Size:             int(fCacheInfo.Size()),
+		}, nil
+	}
+	return nil, errors.New("nex artifact not found in provided oci manifest")
 }
--- node/actors/artifacts_test.go
+++ node/actors/artifacts_test.go
@@ -1,27 +1,86 @@
 package actors
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"net"
 	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 	"testing"
+	"time"
+
+	"github.com/nats-io/nats-server/v2/server"
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
+	natsregistry "github.com/synadia-labs/oci-registry-nats"
+
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
+	oras "oras.land/oras-go/v2"
+	"oras.land/oras-go/v2/content/file"
+	"oras.land/oras-go/v2/registry/remote"
+	"oras.land/oras-go/v2/registry/remote/auth"
+	"oras.land/oras-go/v2/registry/remote/retry"
 )
 
-func createTestBinary(t testing.TB) string {
+func startNatsServer(t testing.TB) (*server.Server, error) {
 	t.Helper()
 
-	f, err := os.Create(filepath.Join(os.TempDir(), "main.go"))
+	s, err := server.NewServer(&server.Options{
+		Port:      -1,
+		JetStream: true,
+		StoreDir:  t.TempDir(),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		if err := server.Run(s); err != nil {
+			server.PrintAndDie(err.Error())
+		}
+	}()
+
+	time.Sleep(1 * time.Second)
+
+	nc, err := nats.Connect(s.ClientURL())
+	if err != nil {
+		return nil, err
+	}
+	jsCtx, err := jetstream.New(nc)
+	if err != nil {
+		return nil, err
+	}
+	_, err = jsCtx.CreateKeyValue(context.TODO(), jetstream.KeyValueConfig{
+		Bucket: "registry",
+	})
+	if err != nil {
+		return nil, err
+	}
+	nc.Close()
+
+	return s, nil
+}
+
+func createTestBinary(t testing.TB, tmpDir string) string {
+	t.Helper()
+
+	f, err := os.Create(filepath.Join(tmpDir, "main.go"))
 	if err != nil {
 		t.Fatalf("Failed to create Go file: %v", err)
 	}
-	f.WriteString(testProg)
+	_, err = f.WriteString(testProg)
+	if err != nil {
+		t.Errorf("Failed to write to Go file: %v", err)
+	}
 	f.Close()
 
 	// Command to compile the Go code into a binary
-	cmd := exec.Command("go", "build", "-o", filepath.Join(t.TempDir(), "output_binary"), f.Name())
+	cmd := exec.Command("go", "build", "-o", filepath.Join(tmpDir, "output_binary"), f.Name())
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
@@ -30,40 +89,149 @@ func createTestBinary(t testing.TB) string {
 		log.Fatalf("Failed to compile Go code: %v", err)
 	}
 
-	return f.Name()
+	return filepath.Join(tmpDir, "output_binary")
 }
 
-func startOCIRegistry(t testing.TB) string {
+func prepOCIArtifact(t testing.TB, workingDir string) (string, error) {
 	t.Helper()
-	// Start the OCI registry
+	s, err := startNatsServer(t)
+	if err != nil {
+		return "", err
+	}
+
+	nc, err := nats.Connect(s.ClientURL())
+	if err != nil {
+		return "", err
+	}
+
+	// find unused port
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		return "", err
+	}
+	port := listener.Addr().(*net.TCPAddr).Port
+	listener.Close()
+
+	nr, err := natsregistry.NewNatsRegistry(nc,
+		natsregistry.WithWebserverAddress(fmt.Sprintf(":%d", port)),
+		// natsregistry.WithLogger(slog.New(slog.NewTextHandler(os.Stdout, nil))),
+	)
+	if err != nil {
+		return "", err
+	}
+
+	go func() {
+		err = nr.Start()
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+	time.Sleep(1 * time.Second)
+
+	repo, err := remote.NewRepository(fmt.Sprintf("localhost:%d/test", port))
+	if err != nil {
+		return "", err
+	}
+	repo.PlainHTTP = true
+	repo.Client = &auth.Client{
+		Client: retry.DefaultClient,
+		Cache:  auth.NewCache(),
+	}
+
+	binPath := createTestBinary(t, workingDir)
+
+	fs, err := file.New(t.TempDir())
+	if err != nil {
+		return "", err
+	}
+	defer fs.Close()
+	ctx := context.Background()
+
+	mediaType := "application/nex.artifact.binary"
+	fileDescriptor, err := fs.Add(ctx, binPath, mediaType, "")
+	if err != nil {
+		return "", err
+	}
+
+	artifactType := "application/nex.artifact"
+	opts := oras.PackManifestOptions{
+		Layers: []v1.Descriptor{fileDescriptor},
+	}
+	manifestDescriptor, err := oras.PackManifest(ctx, fs, oras.PackManifestVersion1_1, artifactType, opts)
+	if err != nil {
+		return "", err
+	}
+
+	tag := "derp"
+	if err = fs.Tag(ctx, manifestDescriptor, tag); err != nil {
+		return "", err
+	}
+
+	_, err = oras.Copy(ctx, fs, tag, repo, tag, oras.DefaultCopyOptions)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("oci://localhost:%d/test:derp", port), nil
+}
+
+func prepNatsObjStoreArtifact(t testing.TB) string {
 	return ""
 }
 
-func startNatsServer(t testing.TB) string {
-	t.Helper()
-	// Start the NATS server
-	return ""
-}
-
-func prepFileArtifact(t testing.TB) string {
+func prepFileArtifact(t testing.TB, workingDir string) string {
 	t.Helper()
 
-	binary := createTestBinary(t)
+	binary := createTestBinary(t, workingDir)
 	return "file://" + binary
 }
 
-func TestOCIArtiface(t *testing.T) {
-	startOCIRegistry(t)
-	// Run the test
+func TestOCIArtifact(t *testing.T) {
+	workingDir := t.TempDir()
+	uri, err := prepOCIArtifact(t, workingDir)
+	if err != nil {
+		t.Fatalf("Failed to prep OCI artifact: %v", err)
+	}
+
+	ref, err := getArtifact("testoci", uri)
+	if err != nil {
+		t.Fatalf("Failed to get artifact: %v", err)
+	}
+	defer os.Remove(ref.LocalCachePath)
+
+	if ref.Name != "testoci" {
+		t.Errorf("expected %s, got %s", "testoci", ref.Name)
+	}
+	if ref.Tag != "derp" {
+		t.Errorf("expected %s, got %s", "derp", ref.Tag)
+	}
+	if ref.OriginalLocation.String() != uri {
+		t.Errorf("expected %s, got %s", uri, ref.OriginalLocation.String())
+	}
+	if !strings.HasPrefix(ref.LocalCachePath, os.TempDir()+"/workload-") {
+		t.Errorf("expected %s, got %s", os.TempDir()+"/workload-", ref.LocalCachePath)
+	}
+	// if ref.Digest != "b92aaf4823a9ffd5aa76f8af00708b60cd658d9dfbee39d717a566e57912204b" {
+	// 	t.Errorf("expected %s, got %s", "b92aaf4823a9ffd5aa76f8af00708b60cd658d9dfbee39d717a566e57912204b", ref.Digest)
+	// }
+	if ref.Size != 2276655 {
+		t.Errorf("expected %d, got %d", 2276655, ref.Size)
+	}
 }
 
 func TestNatsArtifact(t *testing.T) {
-	startNatsServer(t)
+	//startNatsServer(t)
 	// Run the test
 }
 
 func TestFileArtifact(t *testing.T) {
-	uri := prepFileArtifact(t)
+	workingDir, err := os.MkdirTemp(os.TempDir(), "nex-test-working-dir")
+	if err != nil {
+		t.Errorf("Failed to create temp dir: %v", err)
+	}
+	defer os.RemoveAll(workingDir)
+
+	uri := prepFileArtifact(t, workingDir)
 	ref, err := getArtifact("test", uri)
 	if err != nil {
 		t.Errorf("Failed to get artifact: %v", err)
@@ -82,11 +250,11 @@ func TestFileArtifact(t *testing.T) {
 	if !strings.HasPrefix(ref.LocalCachePath, os.TempDir()+"/workload-") {
 		t.Errorf("expected %s, got %s", os.TempDir()+"/workload-", ref.LocalCachePath)
 	}
-	if ref.Digest != "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855" {
-		t.Errorf("expected %s, got %s", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", ref.Digest)
-	}
-	if ref.Size != 0 {
-		t.Errorf("expected %d, got %d", 0, ref.Size)
+	// if ref.Digest != "bcc1d22e206389acfcf479f2ae4f2042e063887296be45351576c8e3996cf483" {
+	// 	t.Errorf("expected %s, got %s", "bcc1d22e206389acfcf479f2ae4f2042e063887296be45351576c8e3996cf483", ref.Digest)
+	// }
+	if ref.Size != 2276647 {
+		t.Errorf("expected %d, got %d", 2276647, ref.Size)
 	}
 }
 
@@ -104,6 +272,7 @@ func TestTagCalculator(t *testing.T) {
 		// tests with a tag
 		{"file:///tmp/foo:derp", "/tmp/foo", "derp", nil},
 		{"oci://synadia/foo:derp", "/foo", "derp", nil},
+		{"oci://synadia:8000/foo:derp", "/foo", "derp", nil},
 		{"nats://myobject/foo:derp", "/foo", "derp", nil},
 	}
 
