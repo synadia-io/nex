f2fa62c71edf6509c2097fabbd3b5a73dec25f5e
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-11-06 09:43:09 -0700

    workload fix
    Signed-off-by: Jordan Rash <15827604+jordan-rash@users.noreply.github.com>

--- test/workload_test.go
+++ test/workload_test.go
@@ -35,55 +35,15 @@ func buildDirectStartBinary(t testing.TB, workingDir string) (string, error) {
 	return filepath.Join(workingDir, "test"), nil
 }
 
-func runCmdOnNode(t testing.TB, ctx context.Context, workingDir, seed, natsURL string, inCmd *exec.Cmd) ([]byte, []byte, error) {
-	t.Helper()
-
-	cmd, err := startNexNodeCmd(t, workingDir, seed, natsURL, "node", "nexus")
-	if err != nil {
-		return nil, nil, err
-	}
-
-	stdout := new(bytes.Buffer)
-	stderr := new(bytes.Buffer)
-	// cmd.Stdout = stdout
-	// cmd.Stderr = stderr
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	err = cmd.Start()
-	if err != nil {
-		return nil, nil, err
-	}
-
-	time.Sleep(500 * time.Millisecond)
-
-	go func() {
-		<-ctx.Done()
-		_ = cmd.Process.Signal(syscall.SIGINT)
-	}()
-
-	go func() {
-		err := inCmd.Run()
-		if err != nil {
-			t.Error(err)
-		}
-		_ = cmd.Process.Signal(syscall.SIGINT)
-	}()
-
-	err = cmd.Wait()
-	if err != nil {
-		t.Log(4)
-		return nil, nil, err
-	}
-
-	return stdout.Bytes(), stderr.Bytes(), nil
-}
-
 func TestDirectStart(t *testing.T) {
 	workingDir := t.TempDir()
-	t.Log(workingDir)
 
 	binPath, err := buildDirectStartBinary(t, workingDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	nexCli, err := buildNexCli(t, workingDir)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -93,12 +53,6 @@ func TestDirectStart(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer s.Shutdown()
-
-	nexCli, err := buildNexCli(t, workingDir)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Log(nexCli)
 
 	kp, err := nkeys.CreateServer()
 	if err != nil {
@@ -110,56 +64,87 @@ func TestDirectStart(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	pub, err := kp.PublicKey()
+	cmd, err := startNexNodeCmd(t, workingDir, string(seed), s.ClientURL(), "node", "nexus")
 	if err != nil {
 		t.Fatal(err)
 	}
+	stdout := new(bytes.Buffer)
+	stderr := new(bytes.Buffer)
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+	// cmd.Stdout = os.Stdout
+	// cmd.Stderr = os.Stderr
 
-	cmd := exec.Command(nexCli, "workload", "run", "-s", s.ClientURL(), "--name", "tester", "--uri", "file://"+binPath, "--node-id", pub)
-	cmdstdout := new(bytes.Buffer)
-	cmdstderr := new(bytes.Buffer)
-	cmd.Stdout = cmdstdout
-	cmd.Stderr = cmdstderr
+	err = cmd.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+	time.Sleep(500 * time.Millisecond)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	_, nodeerr, err := runCmdOnNode(t, ctx, workingDir, string(seed), s.ClientURL(), cmd)
+
+	go func() {
+		<-ctx.Done()
+		_ = cmd.Process.Signal(syscall.SIGINT)
+	}()
+
+	go func() {
+		pub, err := kp.PublicKey()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		cmd := exec.Command(nexCli, "workload", "run", "-s", s.ClientURL(), "--name", "tester", "--uri", "file://"+binPath, "--node-id", pub)
+		cmdstdout := new(bytes.Buffer)
+		cmdstderr := new(bytes.Buffer)
+		cmd.Stdout = cmdstdout
+		cmd.Stderr = cmdstderr
+		err = cmd.Run()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		if len(cmdstderr.Bytes()) > 0 {
+			t.Error("stderr:", cmdstderr.String())
+			return
+		}
+
+		re := regexp.MustCompile(`^Workload tester \[(?P<workload>[A-Za-z0-9]+)\] started on node (?P<node>[A-Z0-9]+)$`)
+		match := re.FindStringSubmatch(strings.TrimSpace(cmdstdout.String()))
+		if len(match) != 3 {
+			t.Error("tester workload failed to start: ", cmdstdout.String())
+			return
+		}
+
+		if pub != match[2] {
+			t.Error("expected node id", pub, "got", match[2])
+		}
+
+		resp, err := http.Get("http://localhost:8087")
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		if string(body) != "passing" {
+			t.Error("expected passing, got", string(body))
+		}
+
+		cancel()
+	}()
+
+	err = cmd.Wait()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if len(nodeerr) > 0 {
-		t.Log("nodeerr: ", string(nodeerr))
-		t.Error("nodeerr should be empty", string(nodeerr))
-		return
-	}
-
-	re := regexp.MustCompile(`^Workload tester \[(?P<workload>[A-Za-z0-9]+)\] started on node (?P<node>[A-Z0-9]+)$`)
-	match := re.FindStringSubmatch(strings.TrimSpace(cmdstdout.String()))
-	if len(match) != 3 {
-		t.Error("tester workload failed to start: ", cmdstdout.String())
-		return
-	}
-
-	if pub != match[2] {
-		t.Error("expected node id", pub, "got", match[2])
-	}
-
-	resp, err := http.Get("http://localhost:8087")
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	if string(body) != "passing" {
-		t.Error("expected passing, got", string(body))
-	}
-
-	// TODO: stop workload
 }
