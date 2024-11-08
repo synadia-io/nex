81c2a7b0d3ee36fa3360c56d4bb7f336218ef2ee
Kevin Hoffman <autodidaddict@users.noreply.github.com>
2024-11-08 08:48:59 -0500

    pre-merge safety commit (I have no idea what I'm doing)

---
+++ api/host_services/service.go
@@ -0,0 +1,63 @@
+package hostservices
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/nats-io/nats.go"
+)
+
+const (
+	headerCode    = "x-nex-hs-code"
+	headerMessage = "x-nex-hs-msg"
+	messageOk     = "OK"
+
+	DefaultConnection      = "default"
+	HostServicesConnection = "hostservices"
+)
+
+type ServiceResult struct {
+	Code    uint
+	Message string
+	Data    []byte
+}
+
+func (r ServiceResult) Error() error {
+	return fmt.Errorf("Error: %s (%d)", r.Message, r.Code)
+}
+
+func (r ServiceResult) IsError() bool {
+	return r.Code != 200
+}
+
+func ServiceResultFail(code uint, message string) ServiceResult {
+	return ServiceResult{
+		Code:    code,
+		Message: message,
+		Data:    []byte{},
+	}
+}
+
+func ServiceResultPass(code uint, message string, data []byte) ServiceResult {
+	return ServiceResult{
+		Code:    code,
+		Message: message,
+		Data:    data,
+	}
+}
+
+type HostService interface {
+	Initialize(json.RawMessage) error
+
+	HandleRequest(
+		connections map[string]*nats.Conn,
+		namespace string,
+		workloadId string,
+		method string,
+		workloadName string,
+		metadata map[string]string,
+		request []byte,
+	) (ServiceResult, error)
+}
+
+const ()
