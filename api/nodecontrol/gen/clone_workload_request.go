ca9b8e5f0810bc7115e2a285df515e4ffb8c1f5e
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2024-11-22 13:30:08 -0700

    fix json codegen
    Signed-off-by: Jordan Rash <15827604+jordan-rash@users.noreply.github.com>

--- api/nodecontrol/gen/clone_workload_request.go
+++ api/nodecontrol/gen/clone_workload_request.go
@@ -27,9 +27,3 @@ func (j *CloneWorkloadRequestJson) UnmarshalJSON(b []byte) error {
 	*j = CloneWorkloadRequestJson(plain)
 	return nil
 }
-
-type CloneWorkloadResponseJson struct {
-	// StartWorkloadRequest corresponds to the JSON schema field
-	// "start_workload_request".
-	StartWorkloadRequest *StartWorkloadRequestJson `json:"start_workload_request,omitempty" yaml:"start_workload_request,omitempty" mapstructure:"start_workload_request,omitempty"`
-}
