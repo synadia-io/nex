b5e376bd5e1ce02d4e135dadb6909b9fae47aeeb
Kevin Hoffman <autodidaddict@users.noreply.github.com>
2024-11-22 10:08:13 -0500

    javascript runner

--- agents/javascript/agent.go
+++ agents/javascript/agent.go
@@ -14,10 +14,16 @@ var _ agentcommon.AgentCallback = &JavaScriptAgent{}
 
 type JavaScriptAgent struct {
 	workloads map[string]*agentapigen.StartWorkloadRequestJson
+	runner    *ScriptRunner
+}
+
+func newProvider(workloadId string, allocator vmAllocator) hostServiceProvider {
+	return NewNodeHostServicesProvider(workloadId, allocator)
 }
 
 func NewAgent() (*JavaScriptAgent, error) {
 	return &JavaScriptAgent{
+		runner:    NewScriptRunner(newProvider),
 		workloads: make(map[string]*agentapigen.StartWorkloadRequestJson),
 	}, nil
 }
@@ -39,11 +45,18 @@ func (a *JavaScriptAgent) Preflight() error {
 
 func (a *JavaScriptAgent) StartWorkload(req *agentapigen.StartWorkloadRequestJson) error {
 	a.workloads[req.WorkloadId] = req
+	// TODO: get script file
+
+	// TODO: call AddScript on runner
+
 	return nil
 }
 
 func (a *JavaScriptAgent) StopWorkload(req *agentapigen.StopWorkloadRequestJson) error {
 	delete(a.workloads, req.WorkloadId)
+
+	// TODO: call RemoveScript on runner
+
 	return nil
 }
 
