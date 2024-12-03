c0f88306ed9ee2a664380b485ffabedff43286c8
Kevin Hoffman <autodidaddict@users.noreply.github.com>
2024-12-03 13:34:50 -0500

    set up host services conn and subscribe to trigger subjects (#446)
    * set up host services conn and subscribe to trigger subjects
    
    * changing logic for triggerable

---
+++ api/agent/go/types.go
@@ -0,0 +1,8 @@
+package agentapi
+
+const (
+	WorkloadTypeDirect     = "direct_start"
+	WorkloadTypeMicroVM    = "microvm"
+	WorkloadTypeJavaScript = "javascript"
+	WorkloadTypeWasm       = "wasm"
+)
