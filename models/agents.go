64ca46d5925638f9337d1f4ec3abaa20c2ec4546
Jordan Rash <jordan@synadia.com>
2025-01-06 16:29:30 -0700

    clean up config file
    Signed-off-by: Jordan Rash <jordan@synadia.com>

--- models/agents.go
+++ models/agents.go
@@ -1,7 +1,42 @@
 package models
 
+import "fmt"
+
 const (
 	DirectStartActorName = "direct-start"
 
 	RunRequestKVBucket = "run_request"
 )
+
+// Subject map for internal comms between host and agents
+func AgentAPIRegisterSubject() string {
+	return fmt.Sprintf("%s.%s.REGISTER", AgentAPIPrefix, NodeSystemNamespace)
+}
+
+func AgentAPIHeartbeatSubject(inAgentName string) string {
+	return fmt.Sprintf("%s.%s.%s.HEARTBEAT", AgentAPIPrefix, NodeSystemNamespace, inAgentName)
+}
+
+func AgentAPIStartWorkloadSubject(inAgentName string) string {
+	return fmt.Sprintf("%s.%s.%s.STARTWORKLOAD", AgentAPIPrefix, NodeSystemNamespace, inAgentName)
+}
+
+func AgentAPIStopWorkloadSubject(inAgentName string) string {
+	return fmt.Sprintf("%s.%s.%s.STOPWORKLOAD", AgentAPIPrefix, NodeSystemNamespace, inAgentName)
+}
+
+func AgentAPIQueryWorkloadSubject(inAgentName string) string {
+	return fmt.Sprintf("%s.%s.%s.QUERYWORKLOAD", AgentAPIPrefix, NodeSystemNamespace, inAgentName)
+}
+
+func AgentAPISetLameduckSubject(inAgentName string) string {
+	return fmt.Sprintf("%s.%s.%s.SETLAMEDUCK", AgentAPIPrefix, NodeSystemNamespace, inAgentName)
+}
+
+func AgentAPIPingWorkloadSubject(inAgentName string) string {
+	return fmt.Sprintf("%s.%s.%s.PINGWORKLOAD", AgentAPIPrefix, NodeSystemNamespace, inAgentName)
+}
+
+func AgentAPIPingSubject(inAgentName string) string {
+	return fmt.Sprintf("%s.%s.%s.PING", AgentAPIPrefix, NodeSystemNamespace, inAgentName)
+}
