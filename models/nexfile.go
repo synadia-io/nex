d294833cdc5883ad0579547653912a9a40bb1739
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2025-02-20 15:10:56 -0700

    Enforce Nexfile fields on Unmarshal (#16)
    * enforce fields for nexfile
    
    Signed-off-by: Jordan Rash <jordan@synadia.com>

--- models/nexfile.go
+++ models/nexfile.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 const NexfileName string = "Nexfile"
 
 type Nexfile struct {
@@ -9,4 +14,30 @@ type Nexfile struct {
 	Type         string            `json:"type" yaml:"type"`
 	Lifecycle    string            `json:"lifecycle" yaml:"lifecycle"`
 	StartRequest any               `json:"start_request" yaml:"start_request"`
+}
+
+func (j *Nexfile) UnmarshalJSON(b []byte) error {
+	var raw map[string]interface{}
+	if err := json.Unmarshal(b, &raw); err != nil {
+		return err
+	}
+	if _, ok := raw["name"]; raw != nil && !ok {
+		return fmt.Errorf("field name in Nexfile: required")
+	}
+	if _, ok := raw["type"]; raw != nil && !ok {
+		return fmt.Errorf("field type in Nexfile: required")
+	}
+	if _, ok := raw["lifecycle"]; raw != nil && !ok {
+		return fmt.Errorf("field lifecycle in Nexfile: required")
+	}
+	if _, ok := raw["start_request"]; raw != nil && !ok {
+		return fmt.Errorf("field start_request in Nexfile: required")
+	}
+	type Plain Nexfile
+	var plain Plain
+	if err := json.Unmarshal(b, &plain); err != nil {
+		return err
+	}
+	*j = Nexfile(plain)
+	return nil
 }
